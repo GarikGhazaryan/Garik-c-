3110ede33efad6b3c3cf37534192f6d4e7eb245f
Garik <garikghazaryan1983@gmail.com>
2023-04-02 09:11:27 +0400

    mod

--- file/task_16_by_file.cpp
+++ file/task_16_by_file.cpp
@@ -5,13 +5,13 @@
 
 int main() {
 
-	std::ifstream input("input.txt"); 												// open the input file for reading
+	std::ifstream input("input.txt"); 			// open the input file for reading
     if (!input.is_open()) {
 			std::cout << "Unable to open input file" << std::endl;
         return 1;
     }
 
-	std::ofstream output("output.txt"); 											// open an output file for recording
+	std::ofstream output("output.txt"); 	// open an output file for recording
     if (!output.is_open()) {
 			std::cout << "Unable to open output file" << std::endl;
         return 1;
@@ -19,9 +19,9 @@ int main() {
 	
 
 	std::string line;
-    while (getline(input, line)) { 													//read the file step by step
+    while (getline(input, line)) { 		//read the file step by step
     	try{
-            int num=stoi(line); 													// convert string to integer
+            int num=stoi(line); 			// convert string to integer
 	 		if(num!=0){
                 int tmp=0;
                 while (num != 0) {
@@ -30,15 +30,15 @@ int main() {
                     tmp = (tmp*10+ miavor)*10;
             }
                     std::cout <<"Output data is         "<<tmp << std::endl;
-            		output << tmp <<std::endl; 										// write number to output file
+            		output << tmp <<std::endl; 		// write number to output file
 				}else{
 					throw std::runtime_error("error : num is = 0");
 				}
 		}	
-        catch (const std::invalid_argument& e) { 									// if unable to convert string to integer
+        catch (const std::invalid_argument& e) { 	// if unable to convert string to integer
 				std::cout<< "Invalid input: " <<line<< e.what()<< std::endl; 
         }
-		catch (const std::exception& e)												// if num is = 0
+		catch (const std::exception& e)					// if num is = 0
 		{
 		    std::cout << e.what()<<std::endl;
 		}	
